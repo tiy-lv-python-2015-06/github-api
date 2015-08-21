5f7b4062822b502f8f7f2c249e29705dfee7c40f
Aaron Pentz <apentz49@gmail.com>
2015-08-20 22:00:57 -0700

    starting on css

--- github_api/github_api/urls.py
+++ github_api/github_api/urls.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'gitpage.views.get_profile')
 ]
--- github_api/gitpage/views.py
+++ github_api/gitpage/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+import requests
+
+
+def get_profile(request):
+    response = requests.get('https://api.github.com/users/Apentz49').json()
+    response['repo_things'] = requests.get('https://api.github.com/users/Apentz49/repos').json()
+    return render_to_response(template_name='index.html', context=response)
